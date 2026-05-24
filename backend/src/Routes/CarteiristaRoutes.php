f76d0787e811dc2bc8925a15cacda05694d91138
Sarturi <guilherme.sarturi20@gmail.com>
2026-05-24 13:01:50 -0300

    feat: Adicionando endpoint filtro e estatisticas

--- backend/src/Routes/CarteiristaRoutes.php
+++ backend/src/Routes/CarteiristaRoutes.php
@@ -6,6 +6,9 @@ use Slim\Routing\RouteCollectorProxy;
 return function(RouteCollectorProxy $app){
     $app->group('/carteiristas', function(RouteCollectorProxy $group){
         $group->get('', CarteiristaController::class.':list');
+        // Rotas estáticas DEVEM vir antes de /{uuid} para evitar conflito de roteamento
+        $group->get('/filtro', CarteiristaController::class.':filter');
+        $group->get('/estatisticas', CarteiristaController::class.':statistics');
         $group->get('/{uuid}', CarteiristaController::class.':get');
         $group->post('', CarteiristaController::class.':create');
         $group->put('/{uuid}', CarteiristaController::class.':update');
